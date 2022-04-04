520fcea304d60361a75c88063018d59ab9407c1f
simonpirko <s.pirko@faceclub.app>
2022-04-04 11:29:15 +0300

    Initial commit

---
+++ src/main/java/by/tms/dzen/yandexdzenrestc51/YandexDzenRestC51Application.java
@@ -0,0 +1,13 @@
+package by.tms.dzen.yandexdzenrestc51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexDzenRestC51Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandexDzenRestC51Application.class, args);
+	}
+
+}
---
+++ src/test/java/by/tms/dzen/yandexdzenrestc51/YandexDzenRestC51ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.dzen.yandexdzenrestc51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexDzenRestC51ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
