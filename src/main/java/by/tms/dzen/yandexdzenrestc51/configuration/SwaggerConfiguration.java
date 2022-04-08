d6692a92f7bf7d9e6566db2e8f25deb87fa220e1
gemuz_work <gemuzkm@gmail.com>
2022-04-08 12:01:34 +0300

    add configuration swagger

---
+++ src/main/java/by/tms/dzen/yandexdzenrestc51/configuration/SwaggerConfiguration.java
@@ -0,0 +1,36 @@
+package by.tms.dzen.yandexdzenrestc51.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("by.tms.dzen.yandexdzenrestc51"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Yandex Dzen API",
+                "Yandex Dzen API by TMS-c51 ",
+                "1.0.0 ",
+                "http://localhost:8080",
+                new Contact("Contact the developer", "http://localhost:8080", "admin@localhost.by"),
+                "Find out more about Yandex Dzen", "https://zen.yandex.ru/", Collections.emptyList());
+    }
+}
