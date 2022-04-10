c2ee011c440c5682819b5513ca3ba244708adc03
papaslip <kuchuk.sa17@gmail.com>
2022-04-10 18:21:17 +0300

    add annotation Swagger ApiResponce

--- src/main/java/by/tms/dzen/yandexdzenrestc51/controller/userController.java
+++ src/main/java/by/tms/dzen/yandexdzenrestc51/controller/userController.java
@@ -3,6 +3,7 @@ package by.tms.dzen.yandexdzenrestc51.controller;
 import by.tms.dzen.yandexdzenrestc51.Entity.User;
 import by.tms.dzen.yandexdzenrestc51.exception.InvalidException;
 import by.tms.dzen.yandexdzenrestc51.repository.UserRepository;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.aspectj.weaver.patterns.HasThisTypePatternTriedToSneakInSomeGenericOrParameterizedTypePatternMatchingStuffAnywhereVisitor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,12 +22,13 @@ public class userController {
     @GetMapping("/{username}")
     public ResponseEntity<User> getUser(@PathVariable("username") String username){
         if (username == null | userRepository.findByUsername(username).isEmpty()){
-            throw new InvalidException();
+            return ResponseEntity.badRequest().build();
         }
         User getUser = userRepository.findByUsername(username).get();
         return ResponseEntity.ok(getUser);
     }
 
+    @ApiResponse(responseCode = "405", description = "Invalid input")
     @PostMapping()
     public ResponseEntity<User> save(@Valid @RequestBody User user, BindingResult bindingResult){
         if (bindingResult.hasErrors()){
@@ -36,6 +38,7 @@ public class userController {
         return ResponseEntity.ok(save);
     }
 
+    @ApiResponse(responseCode = "405", description = "Invalid input")
     @PatchMapping("/{username}")
     public ResponseEntity<User> update(@PathVariable("username") String username, @Valid @RequestBody User user, BindingResult bindingResult){
         if (bindingResult.hasErrors()){
