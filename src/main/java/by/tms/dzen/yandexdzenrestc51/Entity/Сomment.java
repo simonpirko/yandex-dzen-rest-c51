29fe52a224ed197ad47e741f0651b902d9fce565
Flatron31 <Akvadivchik@gmail.com>
2022-04-12 15:08:15 +0300

    fix Comment

--- src/main/java/by/tms/dzen/yandexdzenrestc51/Entity/Сomment.java
+++ src/main/java/by/tms/dzen/yandexdzenrestc51/Entity/Сomment.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -18,4 +19,10 @@ public class Сomment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private LocalDateTime createDate;
+    private long countLike;
+    private long countDizlike;
+
+
+
 }
