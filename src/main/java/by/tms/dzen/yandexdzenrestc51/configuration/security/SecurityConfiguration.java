0f1da94eeefa1f91625d66fc417111f4b5290aac
Kerus Malsim <gemuzkm@gmail.com>
2022-05-26 10:33:25 +0300

    global role add

--- src/main/java/by/tms/dzen/yandexdzenrestc51/configuration/security/SecurityConfiguration.java
+++ src/main/java/by/tms/dzen/yandexdzenrestc51/configuration/security/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 @PropertySource("classpath:endpoint.properties")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
